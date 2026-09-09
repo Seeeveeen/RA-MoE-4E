d3905f3ce86183b464be2e558b30f07cfd02b65f
LI YITONG <ccone1226@163.com>
2026-09-09 12:14:42 +0800

    Add training utilities to __init__.py

--- src/ra_moe/training/__init__.py
+++ src/ra_moe/training/__init__.py
@@ -1,5 +1,9 @@
 """Training utilities for RA-MoE-4E."""
 
+from .epoch import (
+    evaluate_model,
+    train_epoch,
+)
 from .losses import (
     entropy_loss,
     load_balance_loss,
@@ -8,4 +12,6 @@ from .losses import (
 __all__ = [
     "entropy_loss",
     "load_balance_loss",
+    "train_epoch",
+    "evaluate_model",
 ]
